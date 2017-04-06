13ae6ad6ea3783381afd75c9e93af4afc12cad7c
Joen Asmussen <joen@automattic.com>
2017-04-06 17:55:01 +0200

    Remove classnames dependency.

--- react/dashicon/inc/index-footer.jsx
+++ react/dashicon/inc/index-footer.jsx
@@ -4,7 +4,7 @@
 			return null;
 		}
 
-		const iconClass = classnames( 'dashicon', icon, className );
+		const iconClass = [ 'dashicon', icon, className ].filter( Boolean ).join( ' ' );
 
 		return (
 			<svg className={ iconClass } xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
