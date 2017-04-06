e7ea23c62183fdac58f7274b7fe43bebbdad8304
Joen Asmussen <joen@automattic.com>
2017-04-06 17:47:38 +0200

    Update build process to fix issues
    This PR seeks to address a few issues:
    
    - A few typos and linting errors in the react component
    - Wrongly parsed titles
    - A rewrite of the react component to be leaner

--- react/dashicon/inc/index-footer.jsx
+++ react/dashicon/inc/index-footer.jsx
@@ -1,5 +1,16 @@
 		}
 
-		return ( svg );
+		if ( ! path ) {
+			return null;
+		}
+
+		const iconClass = classnames( 'dashicon', icon, className );
+
+		return (
+			<svg className={ iconClass } xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+				{ title ? <title>{ title }</title> : null }
+				<path d={ path } />
+			</svg>
+		);
 	}
 }
