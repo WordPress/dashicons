17adf0cc07acbceb7f954b8cf10b8d2daea52476
Joen Asmussen <joen@automattic.com>
2017-06-06 09:38:34 +0200

    Fix typo.

--- react/dashicon/inc/index-footer.jsx
+++ react/dashicon/inc/index-footer.jsx
@@ -7,7 +7,7 @@
 		const iconClass = [ 'dashicon', 'dashicons-' + icon, className ].filter( Boolean ).join( ' ' );
 
 		return (
-			<svg ariaHidden="true" role="img" className={ iconClass } xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+			<svg aria-hidden="true" role="img" className={ iconClass } xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
 				<path d={ path } />
 			</svg>
 		);
