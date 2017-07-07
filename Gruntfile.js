975f7395ad471fbc9ab2f1283cf82ece36f598ee
Folletto <folletto@gmail.com>
2017-07-07 18:14:56 +0100

    WebFont: "optimize: false" setting

--- Gruntfile.js
+++ Gruntfile.js
@@ -80,6 +80,7 @@ module.exports = function( grunt ) {
 				'engine': 'node',
 				'autoHint': false,
 				'normalize': true,
+        'optimize': false,
 				'font': 'dashicons',
 				'types': 'eot,woff2,woff,ttf',
 				'order': 'eot,woff,ttf',
