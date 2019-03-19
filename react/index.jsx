b6daed89c00b4ac8635c8f851111ce46e7636ddc
Jonathan Desrosiers <desrosj@gmail.com>
2019-03-19 09:38:53 -0400

    Rename `plus-light` back to `plus-alt2`.

--- react/index.jsx
+++ react/index.jsx
@@ -683,7 +683,7 @@ export default class Dashicon extends Component {
 			case 'plus-alt':
 				path = 'M15.8 4.2c3.2 3.21 3.2 8.39 0 11.6-3.21 3.2-8.39 3.2-11.6 0C1 12.59 1 7.41 4.2 4.2 7.41 1 12.59 1 15.8 4.2zm-4.3 11.3v-4h4v-3h-4v-4h-3v4h-4v3h4v4h3z';
 				break;
-			case 'plus-light':
+			case 'plus-alt2':
 				path = 'M17 9v2h-6v6H9v-6H3V9h6V3h2v6h6z';
 				break;
 			case 'plus':
